e550852d1ae3dd6feccb6b67ba5c7a152654d42f
Jimenez <norma.jimenez@guentner.com>
2018-01-18 17:24:03 -0600

    "Login"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginnComponent } from './components/loginn/loginn.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginnComponent
   ],
   imports: [
     BrowserModule
