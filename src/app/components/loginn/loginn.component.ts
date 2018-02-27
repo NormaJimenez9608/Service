82c6a4bc1379505bd79080d865cb77e4b7cf49c4
Jimenez <norma.jimenez@guentner.com>
2018-02-27 16:56:51 -0600

    Redireccion a inicio

--- src/app/components/loginn/loginn.component.ts
+++ src/app/components/loginn/loginn.component.ts
@@ -4,8 +4,7 @@ import { DYNAMIC_TYPE } from '@angular/compiler/src/output/output_ast';
 import {  Login } from '../../models/login';
 import { LoginService } from '../../services/login.service';
 
-//Aqui se importa 
-//En los conceptos basicos del documento de drive se dice como generar el servicio, y como generar el modelo 
+ 
 @Component({
   selector: 'app-loginn',
   templateUrl: './loginn.component.html',
@@ -16,23 +15,27 @@ import { LoginService } from '../../services/login.service';
 })
 export class LoginnComponent implements OnInit {
   login = new Login();
-//dentro de los parentesis se declara lo del constructor 
-  constructor(private username:LoginService) { 
+ 
+
+  constructor(private username:LoginService, private router:Router) { 
 
   }
   ngOnInit() {
   }
-  //aqui se declaran nuevas funciones como la de login(){}
+
 
   Login(){
 const username = this.login.username;
 const password = this.login.password;
-this.username.Login(this.login).subscribe(result=>{}, error=>{});
-if(username == 'Guentner' && password == '123'){
-console.log('succes' +username);
+
+
+this.username.Login(this.login).subscribe(result=>{
+  console.log('HOLA');
+  this.router.navigate(['inicio']);
+}, error=>{
+  alert("Usuario o contraseña incorrecta");
+});
+
+
 }
-else{
-console.log('false'+ username);
-}
-  }
-}
+
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,33 +3,30 @@ import {Login} from '../models/login';
 import {Router} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import {Http, Response, Headers} from '@angular/http';
-
-// aqui se importan
+import {Http, Response, Headers, ResponseContentType, } from '@angular/http';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class LoginService {
-// aqui se declara el headers, username, url
 private headers = new Headers({ 'Content-Type': 'application/json', 'X-Requested-With': 'XMLHttpRequest' });
 
 public username;
 private url;
 
-//dentro de () de constructor se declara http y router
-  constructor (private _http: Http,private router: Router) { 
-    this.url = 'https://api.netbiter.net/operation/v1/rest/json/user/authenticate';     
-  }
+  constructor (public _http: Http,private router: Router) { 
+    this.url = 'https://api.netbiter.net/operation/v1/rest/json/user/authenticate';
 
-  //aqui se crea funcion  login 
+  }
 
 Login(modelologin:Login){
   const body= {
-    'username': modelologin.username,
+    'userName': modelologin.username,
     'password': modelologin.password
 }
 console.log(body);
+
   return this._http.post(this.url, body, {headers: this.headers}).map(res=>res.json());
-}
+   
 }
 
-
+}
