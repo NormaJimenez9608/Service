97e0b7c694e3e355b174a6e53dbad868c3219c8f
Jimenez <norma.jimenez@guentner.com>
2018-07-19 15:39:38 -0500

    cambios en graficas

--- src/app/components/graphics/graphics.component.ts
+++ src/app/components/graphics/graphics.component.ts
@@ -9,6 +9,7 @@ import { constructDependencies } from '@angular/core/src/di/reflective_provider'
 import { MorrisJsModule } from 'angular-morris-js';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Datalog2 } from '../../models/datalog2';
+import { IfObservable } from 'rxjs/observable/IfObservable';
 declare var Morris: any;
 declare var $: any;
 @Component({
@@ -21,27 +22,27 @@ export class GraphicsComponent implements OnInit {
   public accessKey;
   public idSystem;
   public listdata: any[] = [];
-  public startDat;
-  public endDat;
-  public Idvalor;
-  public Idvalor2;
   public listdata1: any[] = [];
   public listdata2: any[] = [];
   public listdata3: any[] = [];
   public listdata4: any[] = [];
   public listdata5: any[] = [];
-  public name;
+  public listdata6: any[] = [];
+  public startDat;
+  public endDat;
+  public name1;
   public name2;
+  public name3;
+  public name4;
+  public name5;
+  public Idvalor;
+  public Idvalor2;
+  public Idvalor3;
+  public Idvalor4;
+  public Idvalor5;
+  public morris1;
   datalog = new Datalog;
   graphics = new Graphics;
-  public morris1;
-
-  public nume;
-  public y;
-
-  onEnter(valueid, value2id){
-    this.getDay(valueid, value2id)
-}
   constructor(private DatalogService: DatalogsService, private router: Router, private datePipe: DatePipe) {
     this.accessKey = localStorage.getItem('accessKey');
     this.idSystem = localStorage.getItem('idSystem');
@@ -49,200 +50,216 @@ export class GraphicsComponent implements OnInit {
 
   ngOnInit() {
     this.getDatalogs();
-
   }
+
   getDatalogs(): void {
     this.DatalogService.getDatalog(this.idSystem, this.accessKey).subscribe((data: any) => {
       this.listdata = data;
-     
       this.startDat = data[data.length - 1].timestamp
       this.endDat = data[0].timestamp;
       this.graphics.startDate = this.datePipe.transform(this.startDat, 'yyyy-MM-dd');
       this.graphics.endDate = this.datePipe.transform(this.endDat, 'yyyy-MM-dd')
-
       this.onChange(data[0].id);
-      this.onChange2(data[0].id);
       this.getGraphics();
     })
   }
-
-  onChange(valueid ) {
-for(let i in this.listdata){
-  if (valueid === this.listdata[i].id)
-  this.name= this.listdata[i].name
-}
-
+  onChange(valueid) {
+    for (let i in this.listdata) {
+      if (valueid === this.listdata[i].id)
+        this.name1 = this.listdata[i].name
+    }
     this.Idvalor = valueid;
-    this.listdata1.length = 0;
-    this.listdata2.length = 0;
-    this.listdata4.length = 0
-    
-    this.getData();
-    
-
+    this.getClean();
+    this.onChange2(this.listdata[0].id);
   }
   onChange2(value2id, ) {
-    for(let i in this.listdata){
+    for (let i in this.listdata) {
       if (value2id === this.listdata[i].id)
-      this.name2= this.listdata[i].name
+        this.name2 = this.listdata[i].name
     }
-  
     this.Idvalor2 = value2id;
-    this.listdata1.length = 0;
-    this.listdata2.length = 0;
-    this.listdata4.length = 0
-    
-    this.getData();
-    
-    
+    this.getClean();
+    this.onChange3(this.listdata[0].id);
+  }
+  onChange3(value3id) {
+    for (let i in this.listdata) {
+      if (value3id === this.listdata[i].id)
+        this.name3 = this.listdata[i].name
+    }
+    this.Idvalor3 = value3id;
+    this.getClean();
+    this.onChange4(this.listdata[0].id);
+  }
+  onChange4(value4id, ) {
+    for (let i in this.listdata) {
+      if (value4id === this.listdata[i].id)
+        this.name4 = this.listdata[i].name
+    }
+    this.Idvalor4 = value4id;
+    this.getClean();
+    this.onChange5(this.listdata[0].id);
+  }
+  onChange5(value5id, ) {
+    for (let i in this.listdata) {
+      if (value5id === this.listdata[i].id)
+        this.name5 = this.listdata[i].name
+    }
+    this.Idvalor5 = value5id;
+    this.getClean();
+    this.getData(this.Idvalor, this.Idvalor2, this.Idvalor3, this.Idvalor4, this.Idvalor5);
   }
 
-
-  getData() {
-    if (this.Idvalor && this.Idvalor2) {
-
-      this.DatalogService.getDetailDay(this.idSystem, this.Idvalor, this.accessKey).subscribe((data: any) => {
-        
-
+  getData(idValor, IdValor2, IdValor3, IdValor4, IdValor5) {
+   
+      this.DatalogService.getDetailDay(this.idSystem, idValor, this.accessKey).subscribe((data: any) => {
         for (let i in data) {
-          this.listdata2.push({
+          this.listdata1.push({
             timestamp: data[i].timestamp,
             value: data[i].maxValue,
           })
         }
-        
-
-        this.DatalogService.getDetailDay(this.idSystem, this.Idvalor2, this.accessKey).subscribe((data2: any) => {
-          
-
+     
+     this.DatalogService.getDetailDay(this.idSystem, IdValor2, this.accessKey).subscribe((data2: any) => {
           for (let i in data2) {
-            this.listdata1.push({
+            this.listdata2.push({
               timestamp: data2[i].timestamp,
               value2: data2[i].maxValue
             })
-          }
-
-          
-          if (this.listdata2.length < this.listdata1.length) {
-            this.startDat = data2[data2.length - 1].timestamp
-           this.endDat = data2[0].timestamp;
-            for (let i in this.listdata1) {
-              
-              if (data[i] == null) {
-                data[i] = {
-                  maxValue: "0",
-                }
-              }
-              this.listdata4.push({
-                timestamp: data2[i].timestamp,
-                value: data[i].maxValue,
-                value2: data2[i].maxValue
+          } 
+     this.DatalogService.getDetailDay(this.idSystem, IdValor3, this.accessKey).subscribe((data3: any) => {
+            for (let i in data3) {
+              this.listdata3.push({
+                timestamp: data3[i].timestamp,
+                value3: data3[i].maxValue
               })
-            }
-          }
-          else {
-            for (let i in this.listdata2) {
-              this.startDat = data[data2.length - 1].timestamp
-              this.endDat = data[0].timestamp;
-              if (data2[i] == null) {
-                data2[i] = {
-                  maxValue: "0",
-                }
+            } 
+     this.DatalogService.getDetailDay(this.idSystem, IdValor4, this.accessKey).subscribe((data4: any) => {
+              for (let i in data4) {
+                this.listdata4.push({
+                  timestamp: data4[i].timestamp,
+                  value4: data4[i].maxValue
+                })
               }
-              this.listdata4.push({
-                timestamp: data[i].timestamp,
-                value: data[i].maxValue,
-                value2: data2[i].maxValue
-              })
-            }
-          }
-          this.graphics.startDate = this.datePipe.transform(this.startDat, 'yyyy-MM-dd');
-           this.graphics.endDate = this.datePipe.transform(this.endDat, 'yyyy-MM-dd')
-          
-          this.morris1.setData(this.listdata4)
-          this.morris1.options.labels =[this.name, this.name2]
-          
-        })
-      })
-    }
-  }
-
-  getDay(valueid, value2id) {
-    this.listdata1.length = 0;
-    this.listdata2.length = 0;
-    this.listdata4.length = 0;
-    const endDate = this.graphics.endDate;
-    const startDate = this.graphics.startDate;
-   // console.log(endDate)
-    this.DatalogService.getDetailDay2(this.idSystem, this.Idvalor, this.accessKey, this.graphics.startDate, this.graphics.endDate).subscribe((data4: any) => {
-    //  console.log(data4);
-      for (let i in data4) {
-        this.listdata2.push({
-          timestamp: data4[i].timestamp,
-          value: data4[i].maxValue,
-        })
-      }
-      
-    
-    this.DatalogService.getDetailDay2(this.idSystem, this.Idvalor2, this.accessKey, this.graphics.startDate, this.graphics.endDate).subscribe((data3: any) => {
-      this.listdata3 = data3;
-    //  console.log(data3)
-      for (let i in data3) {
-        this.listdata1.push({
-          timestamp: data3[i].timestamp,
-          value2: data4[i].maxValue
-        })
-      }
-      if (this.listdata2.length < this.listdata1.length) {
-        for (let i in this.listdata1) {
+     this.DatalogService.getDetailDay(this.idSystem, IdValor5, this.accessKey).subscribe((data5: any) => {
+                for (let i in data5) {
+                  this.listdata5.push({
+                    timestamp: data5[i].timestamp,
+                    value5: data5[i].maxValue
+                  })
+                }
               
-          if (data4[i] == null) {
-            data4[i] = {
+                console.log(this.listdata1.length, this.listdata2.length )
+    //  console.log(this.listdata1, this.listdata2, this.listdata3, this.listdata4, this.listdata5)
+    //  console.log(this.listdata1.length, this.listdata2.length)
+      if(this.listdata1.length > this.listdata2.length ){
+        this.startDat = this.listdata1[this.listdata1.length - 1].timestamp
+        this.endDat = this.listdata1[0].timestamp;
+        for(let i in this.listdata1){
+          if (this.listdata2[i] == null) {
+           this.listdata2[i] = {
               maxValue: "0",
             }
           }
-          this.listdata4.push({
-            timestamp: data3[i].timestamp,
-            value: data3[i].maxValue,
-            value2: data3[i].maxValue
-          })
+          if (this.listdata3[i] == null) {
+            this.listdata3[i] = {
+               maxValue: "0",
+             }
+           }
+           if (this.listdata4[i] == null) {
+            this.listdata4[i] = {
+               maxValue: "0",
+             }
+           }
+           if (this.listdata5[i] == null) {
+            this.listdata5[i] = {
+               maxValue: "0",
+             }
+           }
+          
+            this.listdata6.push({
+              timestamp:this.listdata1[i].timestamp,
+              value: this.listdata1[i].value,
+              value2: this.listdata2[i].value2,
+              value3: this.listdata3[i].value3,
+              value4: this.listdata4[i].value4,
+              value5: this.listdata5[i].value5, 
+            })
+          
         }
       }
-      else {
+      else{
+        this.startDat = this.listdata2[this.listdata2.length - 1].timestamp
+        this.endDat = this.listdata2[0].timestamp;
         for (let i in this.listdata2) {
-          
-          if (data3[i] == null) {
-            data3[i] = {
-              maxValue: "0",
+          if (this.listdata1[i] == null) {
+            this.listdata1[i] = {
+               maxValue: "0",
+             }
+           }
+           if (this.listdata3[i] == null) {
+             this.listdata3[i] = {
+                maxValue: "0",
+              }
             }
-          }
-          this.listdata4.push({
-            timestamp: data4[i].timestamp,
-            value: data4[i].maxValue,
-            value2: data3[i].maxValue
-          })
-        }
-        
-      }
-      this.morris1.setData(this.listdata4)
-      //console.log(this.listdata4)
-    })
-  })
+            if (this.listdata4[i] == null) {
+             this.listdata4[i] = {
+                maxValue: "0",
+              }
+            }
+            if (this.listdata5[i] == null) {
+             this.listdata5[i] = {
+                maxValue: "0",
+              }
+            }
+            this.listdata6.push({
+              timestamp:this.listdata2[i].timestamp,
+              value: this.listdata1[i].value,
+              value2: this.listdata2[i].value2,
+              value3: this.listdata3[i].value3,
+              value4: this.listdata4[i].value4,
+              value5: this.listdata5[i].value5, 
+            })
+         } 
+       }  
+       this.graphics.startDate = this.datePipe.transform(this.startDat, 'yyyy-MM-dd');
+       this.graphics.endDate = this.datePipe.transform(this.endDat, 'yyyy-MM-dd')        
+       this.morris1.setData(this.listdata6)
+       this.morris1.options.labels =[this.name1, this.name2, this.name3, this.name4, this.name5]
+
+       console.log(this.listdata6) 
+        })
+       })
+     })
+   })
+ })    
+}  
+  
+ 
+
+  
+  
+  getClean(){
+    this.listdata1.length =0;
+    this.listdata2.length=0;
+    this.listdata3.length=0;
+    this.listdata4.length=0;
+    this.listdata5.length=0;
+    this.listdata6.length=0;
   }
   getGraphics() {
 
     this.morris1 = new Morris.Line({
       element: 'line-chart',
-      data: this.listdata4,
+      data: this.listdata6,
       xkey: 'timestamp',
-      ykeys: ['value', 'value2'],
-      
-      labels: [this.name, this.name2],
+      ykeys: ['value', 'value2', 'value3', 'value4', 'value5'],
+      labels: [this.name1, this.name2, this.name3, this.name4, this.name5],
       resize: true,
-      lineColors: ['#DA434C', '#696666'],
+      lineColors: ['#DA434C', '#696666', '#F94207', '#CDC403', '#04779E'],
     });
   //  console.log(this.listdata4)
 
   }
+
 }
 
--- src/app/services/datalogs.service.ts
+++ src/app/services/datalogs.service.ts
@@ -30,7 +30,7 @@ getDatalog(idSystems, accessKey:string){
 
 getDetailDay(idSystems, idLog, accessKey){
   this.url2 = `https://api.netbiter.net/operation/v1/rest/json/system/${idSystems}/log/${idLog}/aggregated/day?accesskey=${accessKey}`;
-  //console.log(this.url2);
+ // console.log(this.url2);
   return this.HTTP.get(this.url2)
 }
 
@@ -47,13 +47,13 @@ this.url4= `https://api.netbiter.net/operation/v1/rest/json/system/${idSystems}/
 }
 getDetailHours2(idSystems, idLog, accessKey, startDate, endDate){
   this.url5= `https://api.netbiter.net/operation/v1/rest/json/system/${idSystems}/log/${idLog}/aggregated/hour?accesskey=${accessKey}&startdate=${startDate}:00Z&enddate=${endDate}:00Z`
- // console.log(this.url5);
+  //console.log(this.url5);
     return this.HTTP.get(this.url5)
   }
 
   getAllDatalog(idSystems, idLog, accessKey){
    this.url6=` https://api.netbiter.net/operation/v1/rest/json/system/${idSystems}/log/${idLog}?accesskey=${accessKey}`
-  //  console.log(this.url6);
+   // console.log(this.url6);
    return this.HTTP.get(this.url6)
   }
 }
